bb1b320337857d72641d1c13aa422de0979d7275
Madsbert <mberthelsen@live.dk>
2025-03-17 13:22:28 +0100

    Made changes to names

--- src/main/java/org/example/binproject/TrashController.java
+++ src/main/java/org/example/binproject/TrashController.java
@@ -3,7 +3,7 @@ package org.example.binproject;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class TrashController {
     @FXML
     private Label welcomeText;
 
