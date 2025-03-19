b12c3aca7e3c34c1c4272ffe6c374b919a8c5006
Madsbert <mberthelsen@live.dk>
2025-03-19 09:00:45 +0100

    shit doesn't work

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -13,9 +13,10 @@ module org.example.binproject {
     requires java.sql;
 
     opens org.example.binproject to javafx.fxml;
+    opens org.example.binproject.Controllers to javafx.fxml;
+    opens org.example.binproject.GUI to javafx.fxml;
+
     exports org.example.binproject;
     exports org.example.binproject.Controllers;
-    opens org.example.binproject.Controllers to javafx.fxml;
     exports org.example.binproject.GUI;
-    opens org.example.binproject.GUI to javafx.fxml;
 }
--- src/main/java/org/example/binproject/GUI/MainScreen.java
+++ src/main/java/org/example/binproject/GUI/MainScreen.java
@@ -2,6 +2,7 @@ package org.example.binproject.GUI;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.example.binproject.Foundation.SQLManager;
@@ -12,7 +13,8 @@ import java.sql.Connection;
 public class MainScreen extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainScreen.class.getResource("Main.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/resources/org/example/binproject/Main.fxml"));
+        Parent root = fxmlLoader.load();
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
@@ -21,6 +23,7 @@ public class MainScreen extends Application {
     }
 
     public static void main(String[] args) throws Exception {
+        //System.out.println(MainScreen.class.getResource("/org/example/binproject/Main.fxml"));
         launch();
         //test connection
         Connection connection = SQLManager.getConnection();
